549df4a3ca9ac9a194f090d97656e2f99d2b4246
shobhikabharti <shobhikabharti@gmail.com>
2021-12-25 20:38:39 +0530

    committed

---
+++ demo/src/main/java/merge/MergeReal.java
@@ -0,0 +1,35 @@
+package merge;
+
+public class MergeReal {
+	
+	String gender;
+	String color;
+	String creed;
+	String race;
+	
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getCreed() {
+		return creed;
+	}
+	public void setCreed(String creed) {
+		this.creed = creed;
+	}
+	public String getRace() {
+		return race;
+	}
+	public void setRace(String race) {
+		this.race = race;
+	}
+
+}
---
+++ demo/src/main/java/merge/MergeTest.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package merge;
+
+/**
+ * @author shobhikabharti
+ *
+ */
+public class MergeTest {
+	
+	String alien;
+	String planet;
+	String universe;
+	String aircraft;
+	
+	public String getAlien() {
+		return alien;
+	}
+	public void setAlien(String alien) {
+		this.alien = alien;
+	}
+	public String getPlanet() {
+		return planet;
+	}
+	public void setPlanet(String planet) {
+		this.planet = planet;
+	}
+	public String getUniverse() {
+		return universe;
+	}
+	public void setUniverse(String universe) {
+		this.universe = universe;
+	}
+	public String getAircraft() {
+		return aircraft;
+	}
+	public void setAircraft(String aircraft) {
+		this.aircraft = aircraft;
+	}
+
+}
